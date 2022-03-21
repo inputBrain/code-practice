56755ccc2f6e5dd4575c356d6057e661f5368d43
Alexander <vsany@meta.ua>
2022-03-21 10:49:24 +0200

    [best practices]

--- Everything/Program.cs
+++ Everything/Program.cs
@@ -1,19 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// int DigitalRoot(long n)
-// {
-    // var cArr = n.ToString().ToCharArray();
-
-    // var num = cArr.Sum(c => long.Parse(c.ToString()));
-
-    // Console.WriteLine(num);
-    // return 0;
-// } ok
-
-
-
-
-
 // 16  -->  1 + 6 = 7
 // 942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
 // 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
@@ -33,4 +19,10 @@ int DigitalRoot(long n)
     }
     
     return DigitalRoot(result);
+}
+
+//BEST PRACTICES:
+int BestPractices(long n)
+{
+    return (int)(1 + (n - 1) % 9);
 }
