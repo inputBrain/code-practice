c07a7fa8cc5558e89c7b2bbf78c1e84e73659d28
Alexander <vsany@meta.ua>
2022-03-22 13:48:15 +0200

    [+] tests. ok

--- CodeTest/CodeTest.cs
+++ CodeTest/CodeTest.cs
@@ -6,14 +6,35 @@ namespace CodeTest;
 public class CodeTest
 {
     [Fact]
-    public void OkTest()
+    public void IsogramTest()
     {
         var isogram = Factory.Isogram();
 
         var isIsogram = isogram.IsIsogram("moOse");
-        var bestPractices = isogram.IsIsogram("moOse", true);
+        var bestPractices = isogram.BestPractices("moOse");
         
         Assert.False(isIsogram);
         Assert.False(bestPractices);
+    }
+
+    [Fact]
+    public void DigitalRootTest()
+    {
+        var digital = Factory.DigitalRoot();
+
+        var result = digital.DigitalRoot(16);
+        Assert.Equal(7, result);
+
+        result = digital.DigitalRoot(942);
+        Assert.Equal(6, result);
+
+        result = digital.DigitalRoot(132189);
+        Assert.Equal(6, result);
+
+        result = digital.DigitalRoot(493193);
+        Assert.Equal(2, result);
+
+        var bestPractices = digital.BestPractices(493193);
+        Assert.Equal(2, bestPractices);
     }
 }
