bb1cc6b84599b274958ec7891f4c0fd598071666
Alexander <vsany@meta.ua>
2022-09-03 23:15:41 +0300

    added converting text to alphabet positions and vice versa

--- CodePractice/AlphabetPositionMain.cs
+++ CodePractice/AlphabetPositionMain.cs
@@ -4,7 +4,7 @@ public class AlphabetPositionMain
 {
     private const string Alphabet = "abcdefghijklmnopqrstuvwxyz";
     
-    public string AlphabetPosition(string text)
+    public string TextToAlphabetPositions(string text)
     {
         var indexCollection = new List<int>();
 
@@ -17,8 +17,29 @@ public class AlphabetPositionMain
             }
         }
 
+        Console.WriteLine(string.Join(" ", indexCollection));
         return string.Join(" ", indexCollection);
         
         // return string.Join(" ", text.ToLower().Select(letter => Alphabet.IndexOf(letter) + 1).Where(index => index > 0).ToList());
+    }
+
+    public string AlphabetPositionsToText(List<int> alphabetPositions)
+    {
+        var preparedString = string.Empty;
+
+        foreach (var position in alphabetPositions)
+        {
+            foreach (var alp in Alphabet)
+            {
+                var index = Alphabet.IndexOf(alp) + 1;
+                if (position == index)
+                {
+                    preparedString += alp;
+                }
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", preparedString));
+        return string.Join(" ", preparedString);
     }
 }
