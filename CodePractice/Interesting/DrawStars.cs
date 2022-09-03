b7bae7e07106d4dc423be756134510ff772564cf
Alexander <vsany@meta.ua>
2022-09-03 23:14:53 +0300

    added DrawStars method

---
+++ CodePractice/Interesting/DrawStars.cs
@@ -0,0 +1,24 @@
+namespace CodePractice.Interesting;
+
+public class DrawStars
+{
+    public static void DrawStarsMain(int totalStars)
+    {
+        if (totalStars <= 0)
+        {
+            Console.WriteLine("Dude, it's over our logic! :D");
+        }
+
+        var maxStarsInOneLine = totalStars / 2;
+        var starCollection = new List<string>();
+
+        for (var i = 0; i < maxStarsInOneLine; i++)
+        {
+            for (var j = 0; j < i; j++)
+            {
+                var result = string.Concat(Enumerable.Repeat("*", j));
+                starCollection.Add(result);
+            }
+        }
+    }
+}
