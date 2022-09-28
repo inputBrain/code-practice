d54a3c4341ebb6e3c71220a3a7e823b4e15f74a9
Alexander <vsany@meta.ua>
2022-09-28 23:36:07 +0300

    in work

--- CodePractice/Program.cs
+++ CodePractice/Program.cs
@@ -1,8 +1,46 @@
-﻿
+﻿using CodePractice.Reverse;
 
 
+// ReverseText.ReverseWords("Один! Два!");
 
+var message = new Message("бла-бла-бла ПриВеТ");
+var update = new Update(message);
 
-using CodePractice.Reverse;
+var myBot = new MyBot();
+myBot.Update(update);
 
-ReverseText.ReverseWords("English? speak you Do");
+class MyBot
+{
+    public Task Update(Update update)
+    {
+        var preparedMessage = update.Message.Sms.ToLower();
+
+        if (preparedMessage.Contains("привет") || preparedMessage.Contains("/start"))
+        {
+            Console.WriteLine($"User's message contains: {preparedMessage}");
+        }
+        
+
+        return Task.Delay(100);
+    }
+}
+
+class Message
+{
+    public string Sms { get; set; }
+
+    public Message(string sms)
+    {
+        Sms = sms;
+    }
+}
+
+class Update
+{
+    public Message Message { get; set; }
+
+    public Update(Message message)
+    {
+        Message = message;
+    }
+}
