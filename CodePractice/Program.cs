da23df32d6b759410b9997027aab548cd8a4afb1
Alexander <vsany@meta.ua>
2022-11-16 05:27:44 +0200

    program

--- CodePractice/Program.cs
+++ CodePractice/Program.cs
@@ -1,46 +1,9 @@
-﻿using CodePractice.Reverse;
+﻿using CodePractice.Alphabet;
+using CodePractice.Reverse;
+using CodePractice.ScoreCheck;
+using CodePractice.StructClass;
 
 
 // ReverseText.ReverseWords("Один! Два!");
 
-var message = new Message("бла-бла-бла ПриВеТ");
-var update = new Update(message);
-
-var myBot = new MyBot();
-myBot.Update(update);
-
-class MyBot
-{
-    public Task Update(Update update)
-    {
-        var preparedMessage = update.Message.Sms.ToLower();
-
-        if (preparedMessage.Contains("привет") || preparedMessage.Contains("/start"))
-        {
-            Console.WriteLine($"User's message contains: {preparedMessage}");
-        }
-        
-
-        return Task.Delay(100);
-    }
-}
-
-class Message
-{
-    public string Sms { get; set; }
-
-    public Message(string sms)
-    {
-        Sms = sms;
-    }
-}
-
-class Update
-{
-    public Message Message { get; set; }
-
-    public Update(Message message)
-    {
-        Message = message;
-    }
-}
+await MainStructClass.Main();
